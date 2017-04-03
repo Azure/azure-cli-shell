2a808a89eb8932bc79fb9b2662ba6155217ac9c4
Courtney Oka <oka.c@husky.neu.edu>
2017-04-03 16:54:32 -0700

    added history

--- History.rst
+++ History.rst
@@ -1,1 +1,10 @@
+.. :changelog:
+
+Release History
+===============
+
+0.1.1
+++++++++++++++++++
+
+* Preview release
 
--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ DEPENDENCIES = [
 
 with open('README.rst', 'r', encoding='utf-8') as f:
     README = f.read()
+with open('HISTORY.rst', 'r', encoding='utf-8') as f:
+    HISTORY = f.read()
 
 __version__ = '0.1.1a27'
 
@@ -23,7 +25,7 @@ setup(
     name='azure-cli-shell',
     version=__version__,
     description='Microsoft Azure Command-Line Interactive Shell',
-    long_description=README + '\n\n',
+    long_description=README + '\n\n' + HISTORY,
     license='MIT',
     author='Microsoft Corporation',
     author_email='azpycli@microsoft.com',
