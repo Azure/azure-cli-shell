4d112289bd28eb077c695826a0b2be60b125c8fc
Courtney Oka <oka.c@husky.neu.edu>
2017-04-03 17:10:55 -0700

    linter

--- build_in_travis.sh
+++ build_in_travis.sh
@@ -10,7 +10,7 @@ set -e
 # PyLint does not yet support Python 3.6 https://github.com/PyCQA/pylint/issues/1241
 if [ "$TRAVIS_PYTHON_VERSION" != "3.6" ]; then
     python -m unittest discover -s test
-    # python -m linter.run "$@"
+    python -m linter.run "$@"
 
 else
     python -m unittest discover -s test -p 'test_*.py'
