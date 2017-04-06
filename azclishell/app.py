47ec56d1dc8e0f0895ecb1b066af58225bb6fe71
Courtney Oka <oka.c@husky.neu.edu>
2017-04-06 13:38:28 -0700

    fixes issue #33

--- azclishell/app.py
+++ azclishell/app.py
@@ -393,7 +393,7 @@ class Shell(object):
         if self.default_command:
             cmd = self.default_command + " " + cmd
 
-        if text.strip() == "quit" or text.strip() == "exit":
+        if cmd.strip() == "quit" or cmd.strip() == "exit":
             break_flag = True
         elif text.strip() == "clear":  # clears the history, but only when you restart
             outside = True
