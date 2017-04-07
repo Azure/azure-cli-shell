8c4d3436bb8e1efe414da75589cc8e98c259bcf6
Courtney Oka <oka.c@husky.neu.edu>
2017-04-07 15:50:28 -0700

    case insensitive az check

--- azclishell/app.py
+++ azclishell/app.py
@@ -387,7 +387,7 @@ class Shell(object):
     def _special_cases(self, text, cmd, outside):
         break_flag = False
         continue_flag = False
-        if text and text.split()[0] == 'az':
+        if text and text.split()[0].lower() == 'az':
             telemetry.track_ssg('az', text)
             cmd = ' '.join(text.split()[1:])
         if self.default_command:
