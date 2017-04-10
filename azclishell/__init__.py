34ea26eb10b10d6a98352a5e36849a706b3ccd3e
Courtney Oka <oka.c@husky.neu.edu>
2017-04-10 11:29:53 -0700

    step by step issues

--- azclishell/__init__.py
+++ azclishell/__init__.py
@@ -2,4 +2,4 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-__version__ = '0.1.1a31'
+__version__ = '0.1.1c1'
