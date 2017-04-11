782aa4a0a741d4f165c776320624d495b954588a
Courtney Oka <oka.c@husky.neu.edu>
2017-04-10 17:25:20 -0700

    added license

--- azclishell/color_styles.py
+++ azclishell/color_styles.py
@@ -1,3 +1,7 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 import platform
 
 from prompt_toolkit.styles import style_from_dict
