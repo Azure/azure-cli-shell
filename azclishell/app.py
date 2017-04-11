b39e51a481dda13df865fe657f48df4c7fb0e3cc
Courtney Oka <oka.c@husky.neu.edu>
2017-04-11 14:11:10 -0700

    meaning with exit code

--- azclishell/app.py
+++ azclishell/app.py
@@ -419,7 +419,11 @@ class Shell(object):
                 telemetry.track_ssg('outside', cmd)
 
             elif text[0] == SELECT_SYMBOL['exit_code']:
-                print(self.last_exit)
+                if self.last_exit == 0:
+                    meaning = 'Success'
+                else:
+                    meaning = 'Failure'
+                print(meaning + ": " + str(self.last_exit))
                 continue_flag = True
                 telemetry.track_ssg('exit code', cmd)
 
