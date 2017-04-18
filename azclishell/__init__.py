1e8d81492588c19a23567ee00daefe582f6ee243
Courtney Oka <oka.c@husky.neu.edu>
2017-04-18 15:45:23 -0700

    version number update

--- azclishell/__init__.py
+++ azclishell/__init__.py
@@ -2,4 +2,4 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-__version__ = '0.2.1'
+__version__ = '0.2.2'
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('README.rst', 'r', encoding='utf-8') as f:
 with open('HISTORY.rst', 'r', encoding='utf-8') as f:
     HISTORY = f.read()
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 setup(
     name='azure-cli-shell',
