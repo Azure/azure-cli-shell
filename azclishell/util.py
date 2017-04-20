febbfe6d45af19df61279d7dbd2511fba903085b
Courtney Oka <oka.c@husky.neu.edu>
2017-04-20 13:44:45 -0700

    removed unnecessary method

--- azclishell/util.py
+++ azclishell/util.py
@@ -52,59 +52,6 @@ def _size_windows():
         return lines, columns
 
 
-def default_style():
-    """ Default coloring """
-    if platform.system() == 'Windows':
-        styles = style_from_dict({
-            # Completion colors
-            Token.Menu.Completions.Completion.Current: 'bg:#7c2c80 #ffffff',
-            Token.Menu.Completions.Completion: 'bg:#00b7b7 #ffffff',
-            Token.Menu.Completions.ProgressButton: 'bg:#b78991',
-            Token.Menu.Completions.ProgressBar: 'bg:#ffc0cb',
-
-            Token.Az: '#7c2c80',
-            Token.Prompt.Arg: '#888888',
-
-            # Pretty Words
-            Token.Keyword: '#965699',
-            Token.Keyword.Declaration: '#ab77ad',
-            Token.Name.Class: '#c49fc5',
-            Token.Text: '#0f5050',
-
-            Token.Line: '#E500E5',
-            Token.Number: '#00ffff',
-            # toolbar
-            Token.Operator: 'bg:#000000 #ffffff',
-            Token.Toolbar: 'bg:#000000 #ffffff'
-        })
-
-    else:
-        styles = style_from_dict({
-            # Completion colors
-            Token.Menu.Completions.Completion.Current: 'bg:#7c2c80 #ffffff',
-            Token.Menu.Completions.Completion: 'bg:#00b7b7 #ffffff',
-            Token.Menu.Completions.ProgressButton: 'bg:#b78991',
-            Token.Menu.Completions.ProgressBar: 'bg:#ffc0cb',
-
-            Token.Az: '#7c2c80',
-            Token.Prompt.Arg: '#888888',
-
-            # Pretty Words
-            Token.Keyword: '#965699',
-            Token.Keyword.Declaration: '#ab77ad',
-            Token.Name.Class: '#c49fc5',
-            Token.Text: '#666666',
-
-            Token.Line: '#E500E5',
-            Token.Number: '#3d79db',
-            # toolbar
-            Token.Operator: 'bg:#000000 #ffffff',
-            Token.Toolbar: 'bg:#000000 #ffffff'
-        })
-
-    return styles
-
-
 def parse_quotes(cmd, quotes=True):
     """ parses quotes """
     string_literals = ['\'', '\"']
